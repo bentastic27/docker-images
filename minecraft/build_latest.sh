bd48cf846b5c7ebee47e03b010dd5b6deb158c42
Ben Healey <bentastic27@gmail.com>
2021-10-07 13:03:52 -0400

    var'ify the repo

--- minecraft/build_latest.sh
+++ minecraft/build_latest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+repo=bentastic27/minecraft
+
 manifest_data=$(curl https://launchermeta.mojang.com/mc/game/version_manifest_v2.json)
 latest_release=$(echo $manifest_data | jq -r '.latest.release')
 latest_release_url=$(echo $manifest_data | jq -r ".versions[] | select(.id == \"${latest_release}\") | .url")
 server_download_url=$(curl $latest_release_url | jq -r '.downloads.server.url')
 
-docker build . -t bentastic27/minecraft:$latest_release --build-arg server_download_url=$server_download_url
-docker tag bentastic27/minecraft:$latest_release bentastic27/minecraft:latest
+docker build . -t $repo:$latest_release --build-arg server_download_url=$server_download_url
+docker tag $repo:$latest_release $repo:latest
